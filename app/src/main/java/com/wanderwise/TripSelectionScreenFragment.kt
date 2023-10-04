10f33bcf577386b49606e8f101c10ddd99428582
VitorCaversan <joaopassos@alunos.utfpr.edu.br>
2023-10-03 23:53:44 -0300

    Adds a lot of colors and icons to the project.

--- app/src/main/java/com/wanderwise/TripSelectionScreenFragment.kt
+++ app/src/main/java/com/wanderwise/TripSelectionScreenFragment.kt
@@ -10,7 +10,12 @@ class TripSelectionScreenFragment : Fragment(R.layout.activity_trip_selection) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val tripButton: Button = view.findViewById(R.id.trip_button)
+        val addTripBtn: Button = view.findViewById(R.id.addTripBtn)
+
         tripButton.setOnClickListener {
+            findNavController().navigate(R.id.action_to_citySelection)
+        }
+        addTripBtn.setOnClickListener {
             findNavController().navigate(R.id.action_to_citySelection)
         }
     }
