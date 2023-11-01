1360e8c46054697d965ef03c99cb3d6edfdca33a
Alfons Andrade <alfons@alunos.utfpr.edu.br>
2023-11-01 01:52:21 -0300

    STORY: Adding the necessary compatibility modifications on Trip Class for FireBase implementation (City and Activity not working)

--- app/src/main/java/com/wanderwise/TripSelectionScreenFragment.kt
+++ app/src/main/java/com/wanderwise/TripSelectionScreenFragment.kt
@@ -18,36 +18,39 @@ class TripSelectionScreenFragment : Fragment(R.layout.activity_trip_selection) {
     private lateinit var database: DatabaseReference
     private lateinit var listView: ListView
     private lateinit var tripsEventListener: ValueEventListener
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    private var isTripsLoaded = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         database = FirebaseDatabase.getInstance().getReferenceFromUrl("https://wanderwise-firebase-default-rtdb.firebaseio.com/").child("trips")
-        val addTripBtn: Button = view.findViewById(R.id.addTripBtn) ?: return
-        listView = view.findViewById(R.id.tripsListView)
         tripList = ArrayList()
         loadTripsFromFirebase()
-        if (tripList.isEmpty()) {
-            val fromDateStr: String = "12/06/2023"
-            val toDateStr: String = "22/06/2023"
-            val fromDate: LocalDate = LocalDate.parse(fromDateStr, DATE_FORMAT)
-            val toDate: LocalDate = LocalDate.parse(toDateStr, DATE_FORMAT)
-            val trip = Trip("Japan trip", fromDate, toDate, "A trip to Japan", R.drawable.landscape)
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val addTripBtn: Button = view.findViewById(R.id.addTripBtn) ?: return
+        listView = view.findViewById(R.id.tripsListView)
 
-            tripList.add(trip)
+        if (!isTripsLoaded) {
+            loadTripsFromFirebase()
+            isTripsLoaded = true
+        }
 
-            val fromDateStr2: String = "23/11/2023"
-            val toDateStr2: String = "15/12/2023"
-            val fromDate2: LocalDate = LocalDate.parse(fromDateStr2, DATE_FORMAT)
-            val toDate2: LocalDate = LocalDate.parse(toDateStr2, DATE_FORMAT)
-            val trip2 = Trip("Greece trip", fromDate2, toDate2, "A trip to Greece", R.drawable.landscape)
+        listView.adapter = TripAdapter(requireContext(), tripList)
+        listView.isClickable = true
+        listView.setOnItemClickListener { _, _, position, _ ->
+            findNavController().navigate(R.id.action_to_citySelection)
+        }
 
-            tripList.add(trip2)
+        addTripBtn.setOnClickListener {
+            findNavController().navigate(R.id.action_to_newTrip)
+        }
 
-            try {
-                val bundle: Bundle = requireArguments()
-                treatReceivedData(bundle)
-            } catch (e: IllegalStateException) {
-                e.printStackTrace()
-            }
+        try {
+            val bundle: Bundle = requireArguments()
+            treatReceivedData(bundle)
+        } catch (e: IllegalStateException) {
+            e.printStackTrace()
         }
 
         listView.adapter = TripAdapter(requireContext(), tripList)
@@ -61,27 +64,30 @@ class TripSelectionScreenFragment : Fragment(R.layout.activity_trip_selection) {
         }
     }
 
-    /**
-     * Treats the data received from the previous fragment 
-     */
     private fun treatReceivedData(bundle: Bundle) {
         val trip = bundle.getParcelable<Trip>("newTrip")
 
         if (null != trip) {
             tripList.add(trip)
-            storeTripToFirebase(trip)  // Store the received trip to Firebase
+            storeTripToFirebase(trip)
         }
     }
     private fun loadTripsFromFirebase() {
+        if (::tripsEventListener.isInitialized) {
+            database.removeEventListener(tripsEventListener)
+        }
+
         tripsEventListener = database.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
-                //tripList.clear()
+                val newTrips = HashSet<Trip>()
                 for (snapshot in dataSnapshot.children) {
                     val firebaseTrip = snapshot.getValue(FirebaseTrip::class.java)
                     if (firebaseTrip != null) {
-                        tripList.add(Trip.fromFirebaseTrip(firebaseTrip))
+                        newTrips.add(Trip.fromFirebaseTrip(firebaseTrip))
                     }
                 }
+                tripList.clear()
+                tripList.addAll(newTrips)
                 (listView.adapter as TripAdapter).notifyDataSetChanged()
             }
             override fun onCancelled(databaseError: DatabaseError) {
@@ -89,7 +95,6 @@ class TripSelectionScreenFragment : Fragment(R.layout.activity_trip_selection) {
             }
         })
     }
-
     override fun onDestroyView() {
         super.onDestroyView()
         database.removeEventListener(tripsEventListener)
