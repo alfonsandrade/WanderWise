a9ad896799a64a02dea7dd2821acbf3ec62ff895
Alfons Andrade <alfons@alunos.utfpr.edu.br>
2023-12-09 20:21:25 -0300

    IMPROVEMENT: Modifying Login Screen and Sign up screen fragment

---
+++ app/src/main/java/com/wanderwise/LoginActivity.java
@@ -0,0 +1,6 @@
+package com.wanderwise;
+
+import android.app.Activity;
+
+public class LoginActivity extends Activity {
+}
--- app/src/main/java/com/wanderwise/SignUpFragment.kt
+++ app/src/main/java/com/wanderwise/SignUpFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageButton
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 
@@ -15,7 +16,6 @@ class SignUpFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.activity_signup_data_fill, container, false)
     }
 
@@ -25,15 +25,13 @@ class SignUpFragment : Fragment() {
         val nameEditText: EditText = view.findViewById(R.id.name_edit_text)
         val emailEditText: EditText = view.findViewById(R.id.email_edit_text)
         val passwordEditText: EditText = view.findViewById(R.id.password_edit_text)
-        val signUpButton: Button = view.findViewById(R.id.sign_up_button)
-        val loginButton: Button = view.findViewById(R.id.login_button)
+        val signUpButton: ImageButton = view.findViewById(R.id.confirm_register_button)
+        val backToLoginButton: ImageButton = view.findViewById(R.id.back_button)
 
         signUpButton.setOnClickListener {
-            // Handle the sign-up logic here
         }
 
-        loginButton.setOnClickListener {
-            // Navigate back to the login screen
+        backToLoginButton.setOnClickListener {
             findNavController().navigate(R.id.action_to_login_screen)
         }
     }
